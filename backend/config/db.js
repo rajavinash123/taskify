e303e9ab4a3dbefb1102d66648e3648e7dffdd1b
avina@LAPTOP-30NU834E <avinashkumar@808482@gmail.com>
2026-05-26 15:05:51 +0530

    "fixed runtime crash"

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
 const uri = process.env.MONGO_URI;
   if (!uri) {
     console.error("❌ MONGO_URI is not defined in .env");
-    process.exit(1);
+   
   }
 
   try {
@@ -15,7 +15,7 @@ const uri = process.env.MONGO_URI;
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.error("❌ Database connection failed:", err.message);
-    process.exit(1);
+    
   }
 };
 
